ae606229c37129e5fda14cc2f608e0af5d2d5d6e
Jonas Bernoulli <jonas@bernoul.li>
2017-08-19 17:41:00 +0200

    [PATCH] Add library apel.el whose name matches that of the package apel
    A package should always contain a library with the same name.  One
    benefit of that is that Melpa can then extract the package description
    from the library commentary.
    
    Remove "apel-pkg.el".  The only additional information it contains is a
    version string, which has not ever been bumped since it was first added
    in 2014.
    
    [Emacsmirror] This commit is being rebased onto upstream.

--- apel-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "apel" "10.8"
-  "A Portable Emacs Library provides support for portable Emacs Lisp programs"
-  '((emacs "24.5"))
-  :maintainers '(("Kazuhiro Ito" . "kzhr@d1.dion.ne.jp")))
