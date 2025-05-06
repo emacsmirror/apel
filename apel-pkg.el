1674816d22348b3f6de2e0c890644f0eee1703d3
Kazuhiro Ito <kzhr@d1.dion.ne.jp>
2025-05-06 14:04:10 +0900

    * apel-pkg.el: Add :maintainers property.

--- apel-pkg.el
+++ apel-pkg.el
@@ -1,3 +1,4 @@
 (define-package "apel" "10.8"
   "A Portable Emacs Library provides support for portable Emacs Lisp programs"
-  '((emacs "24.5")))
+  '((emacs "24.5"))
+  :maintainers '(("Kazuhiro Ito" . "kzhr@d1.dion.ne.jp")))
